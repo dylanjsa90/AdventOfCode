71e28ac262269486ff21ca225172ff4784c3c99d
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-05 00:20:13 -0800

    day part 1 solution, used the command line, will refactor with reading the problem from a file

--- adventCode.js
+++ adventCode.js
@@ -1,2 +1,31 @@
 'use strict';
 
+calcDistance(process.argv.slice(2));
+
+function calcDistance(input) {
+  var inputArr = input[0].split(', ');
+  var facing = 'N';
+  var dirKey = {'N': 0, 'E': 1, 'S': 2, 'W': 3};
+  var dirTotals = {'N': 0, 'E': 0, 'S': 0, 'W': 0};
+  var directionArr = ['N', 'E', 'S', 'W'];
+  for (var i = 0; i < inputArr.length; i++) {
+    var currDirection = inputArr[i].charAt(0);
+    if (currDirection === 'R') {
+      if (facing === 'W') {
+        facing = 'N';
+      } else {
+        facing = directionArr[dirKey[facing] + 1];
+      }
+    } else if (currDirection === 'L') {
+      if (facing === 'N') {
+        facing = 'W';
+      } else {
+        facing = directionArr[dirKey[facing] - 1];
+      }
+    }
+
+    dirTotals[facing] += parseInt(inputArr[i].substring(1)); 
+  }
+  var dist = Math.abs(dirTotals['N'] - dirTotals['S']) + Math.abs(dirTotals['W'] - dirTotals['E']);
+  console.log(dist);
+}
